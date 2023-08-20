bce1eaa2337dc15af7996fb202d7c689aef121fb
FernandoJungBereza <fernandojung.b@gmail.com>
2023-08-20 20:00:10 -0300

    Corrigido alguns erros de layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,7 @@ import React from 'react'
 export default function Home() {
   return (
     <main className=''>
-      <div className=''>
         teste
-      </div>
     </main>
   )
 }
--- src/components/patterns/Navbar/navbar.tsx
+++ src/components/patterns/Navbar/navbar.tsx
@@ -24,7 +24,7 @@ export default function MenuNavbar() {
         {
             id: 1,
             path: "/",
-            text: "Home-Hibrid",
+            text: "Hibrid-Render",
             icon: <AiFillHome title="home" size={35} color="white" />,
         },
         {
@@ -44,7 +44,7 @@ export default function MenuNavbar() {
         {
             id: 4,
             path: "/StaticRender",
-            text: "Static",
+            text: "Static-Render",
             icon: < IoIosThunderstorm title="static-render" size={40} color="white" />,
         },
 
@@ -61,7 +61,7 @@ export default function MenuNavbar() {
             <div className={` bg-neutral-900 text-white text-xl border-r-2 border-blue-700 ${menuIsOpen == true ? 'w-[200px]' : 'w-[50px]'} text-black  transition-all duration-200 z-50`}>
                 <div className={`flex flex-col text-xl fixed ${menuIsOpen == true ? 'w-[200px]' : 'w-[50px]'} transition-all duration-200`}>
                     <div className={` ${menuIsOpen == true ? 'h-[60px] border-b-4 px-2' : 'h-[0px]'} border-blue-700 transition-all duration-200 z-30 shadow-black/50 shadow-md`}>
-                        <h1 className={`overflow-hidden whitespace-nowrap ${menuIsOpen == true ? '' : 'hidden'} h-full flex justify-center items-center`}>TCC-Fernando</h1>
+                        <h1 className={`overflow-hidden whitespace-nowrap ${menuIsOpen == true ? '' : 'hidden'} h-full flex justify-center items-center`}>Renderizações</h1>
                     </div>
                     {menu.map((item, index) => {
                         return (
@@ -80,8 +80,8 @@ export default function MenuNavbar() {
                             {
                             pathRoute == '/ServerRender' ? 'Server-Side-Render' 
                             : pathRoute == '/ClientRender' ? 'Client-Side-Render' 
-                            : pathRoute == '/Static' ?'Static-Render' 
-                            : 'Home'
+                            : pathRoute == '/StaticRender' ? 'Static-Render' 
+                            : 'Hibrid-Render'
                             }
                         </h1>
                     </div>
