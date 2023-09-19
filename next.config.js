1d22c5370d74423754ca9ecc0331d49a326e8518
FernandoJungBereza <107483471+FernandoJungBereza@users.noreply.github.com>
2023-09-19 20:30:39 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const nextConfig = {
     reactStrictMode: true,
-    ignoreDuringBuilds: true,
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 };
 
 module.exports = {
