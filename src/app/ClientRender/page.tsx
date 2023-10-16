6cf25c1e97a90e0a096b50662163dad3747b6599
FernandoJungBereza <fernandojung.b@gmail.com>
2023-10-16 03:08:00 -0300

    mudanças tcc

--- src/app/ClientRender/page.tsx
+++ src/app/ClientRender/page.tsx
@@ -3,67 +3,144 @@
 import { ContainerPage } from "@/components/containers/containers"
 import { ArrayRender } from "@/components/pages/arrayRender"
 import { IData } from "@/interfaces"
+import { IDataRender } from "@/interfaces/ServerRender"
 import Image from "next/image"
 import { useState, useEffect } from "react"
 
 export default function ClientRender() {
 
-    const [data, setData] = useState<Array<IData>>()
+    const [data, setData] = useState<IDataRender | null>()
 
-    function JsonServerGet(){
-        fetch('http://localhost:4000/teste')
-        .then(res => {
-            res.json()
-            .then(result =>{
-                setData(result)
+    function JsonServerGet() {
+        fetch('http://localhost:4000/ClientRender')
+            .then(res => {
+                res.json()
+                    .then(result => {
+                        setData(result)
+                    })
+            }).catch(err => {
+                setData(null)
             })
-        }).catch(err => {
-            console.log(`deu ruim`)
-        })
     }
 
     useEffect(() => {
         JsonServerGet()
     }, [])
 
+    console.log(data?.imagem_destaque)
+
     return (
         <div>
-            <title>Client</title>
-            <meta name="description" content="Página para testar a renderização pelo client" />
+            <title>Server Side Render</title>
+            <meta name="description" content="Página para testar a renderização pelo servidor" />
+            {data ?
+                <Image
+                    src={`${data?.imagem_destaque}`}
+                    width={1920}
+                    height={600}
+                    alt={"Culínaria"}
+                /> : null
+            }
             <ContainerPage>
-                <div className="flex justify-center my-10">
-                    <h1 className="text-4xl">Testando a Renderização pelo Client</h1>
-                </div>
-                <div className="space-y-4">
-                    {data?.map((item, index) => {
-                        return (
-                            <div key={item.id + index}>
-                                <ArrayRender
-                                    description={item.caracteristica}
-                                    topic={item.nome}
-                                />
+                <div className="flex-col justify-center w-11/12 m-auto">
+                    <div className="flex justify-center my-10">
+                        <h1 className="text-4xl">{data?.titulo}</h1>
+                    </div>
+                    <div className="space-y-4">
+                        <h2 className="text-2xl flex justify-center">{data?.subtitulo}</h2>
+                    </div>
+                    <div className="py-8">
+                        <p>
+                            Conteudo: {data?.conteudo}
+                        </p>
+                    </div>
+                    <div className="flex w-full justify-between  py-8">
+                        <ul className="marker:text-sky-400 text-center">
+                            <li>Autor: {data?.autor?.nome}</li>
+                            <li>Email: {data?.autor?.email}</li>
+                            <li>Linkedin: {data?.autor?.perfil_social.linkedin}</li>
+                            <li>Twitter: {data?.autor?.perfil_social?.twitter}</li>
+                        </ul>
+                        <div>
+                            <span className="text-center flex justify-center">
+                                Data de Publicação: {data?.data_publicacao}
+                            </span>
+                        </div>
+                        <div className="flex items-center space-x-4">
+                            <label className="text-2xl">tags: </label>
+                            <div className="flex-col">
+                                {data?.tags?.map((item, index) => {
+                                    return (
+                                        <div key={index}>
+                                            {item}
+                                        </div>
+                                    )
+                                })}
                             </div>
-                        )
-                    })}
-                </div>
-                <div className="w-full max-h-[200px] overflow-auto overflow-y-hidden my-8">
-                    {data != undefined &&
-                        <Image src={data[0].midia.nome ?? ''} width={1920} height={600} alt={"ImageServer"} priority={true} />
-                    }
-                </div>
-                <div className="flex-col justify-center mb-8">
-                    {data?.map((item, index) => {
-                        return (
-                            <div key={index + item.id} >
-                                <div className="text-center py-8">
-                                    <h2>{item.title}</h2>
-                                </div>
-                                <div className="w-11/12 m-auto">
-                                    <p>{item.resposta}</p>
-                                </div>
+                        </div>
+                    </div>
+                    <div className="flex justify-between">
+                        <div className="w-full">
+                            <h3 className="text-2xl text-center">
+                                Comentarios
+                            </h3>
+                            <div className="space-y-4 p-2">
+                                {data?.comentarios?.map((item, index) => {
+                                    return (
+                                        <div key={index}>
+                                            <div className="bg-neutral-600 border-2 border-blue-600 rounded-md p-4">
+                                                <ul>
+                                                    <li>Autor: {item.autor_comentario}</li>
+                                                    <li>Email: {item.email_comentario}</li>
+                                                    <li>Data: {item.data_comentario}</li>
+                                                    <li> Comentário: {item.texto_comentario}</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    )
+                                })}
                             </div>
-                        )
-                    })}
+                        </div>
+                        <div className="w-full">
+                            <h3 className="text-2xl text-center">
+                                Artigos Relacionados
+                            </h3>
+                            <div className="space-y-4 p-2">
+                                {data?.artigos_relacionados?.map((item, index) => {
+                                    return (
+                                        <div key={index}>
+                                            <div className="bg-neutral-600 border-2 border-blue-600 rounded-md p-4">
+                                                <ul>
+                                                    <h4 className="text-xl justify-center flex">{item.titulo}</h4>
+                                                    <div className="flex-col">
+                                                        <label className="text-center flex justify-center">Resumo</label>
+                                                        <div className="p-4">{item.resumo}</div>
+                                                    </div>
+                                                    <div className="flex justify-between">
+                                                        <span>Autor: {item.autor}</span>
+                                                        <span>Data: {item.data_publicacao}</span>
+                                                    </div>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <h5 className="text-2xl py-4">Informações Adicionais</h5>
+                        <ul className="py-4">
+                            <li>Categoria: {data?.categoria}</li>
+                            <li>Visualizações: {data?.visualizacoes}</li>
+                            <li>Tempo de Leitura:  {data?.tempo_leitura}</li>
+                            <li>Idioma: {data?.idioma}</li>
+                            <li>Número de Palavras: {data?.numero_palavras}</li>
+                            <li>Foto do Autor: {data?.autor_foto}</li>
+                            <li>Url de compartilhamento: {data?.url_compartilhamento}</li>
+                            <li>Referencia: {data?.fonte_referencia}</li>
+                        </ul>
+                    </div>
                 </div>
             </ContainerPage>
         </div>
--- src/app/ServerRender/page.tsx
+++ src/app/ServerRender/page.tsx
@@ -1,55 +1,130 @@
 import { ContainerPage } from "@/components/containers/containers"
-import { ArrayRender } from "@/components/pages/arrayRender"
-import { IData } from "@/interfaces"
-import Head from "next/head"
+import { IDataRender } from "@/interfaces/ServerRender"
 import Image from "next/image"
-import Link from "next/link"
 
 export default async function ServerRender() {
-    // const res = await fetch('http://localhost:4000/teste', {
-    //     next: {
+    let data: IDataRender | null
 
-    //     }
-    // })
-    // const data: Array<IData> = await res.json()
+    try {
+        const res = await fetch('http://localhost:4000/ServerRender', {
+            cache: "no-cache"
+        })
+        data = await res.json()
+    } catch {
+        data = null
+    }
 
     return (
         <div>
             <title>Server Side Render</title>
             <meta name="description" content="Página para testar a renderização pelo servidor" />
+            <Image
+                src={`${data?.imagem_destaque}`}
+                width={1920}
+                height={600}
+                alt={"Culínaria"}
+            />
             <ContainerPage>
-                <div className="flex justify-center my-10">
-                    <h1 className="text-4xl">Testando a Renderização pelo Servidor</h1>
-                </div>
-                <div className="space-y-4">
-                    {/* {data.map((item, index) => {
-                        return (
-                            <div key={item.id + index}>
-                                <ArrayRender
-                                    description={item.caracteristica}
-                                    topic={item.nome}
-                                />
+                <div className="flex-col justify-center w-11/12 m-auto">
+                    <div className="flex justify-center my-10">
+                        <h1 className="text-4xl">{data?.titulo}</h1>
+                    </div>
+                    <div className="space-y-4">
+                        <h2 className="text-2xl flex justify-center">{data?.subtitulo}</h2>
+                    </div>
+                    <div className="py-8">
+                        <p>
+                            Conteudo: {data?.conteudo}
+                        </p>
+                    </div>
+                    <div className="flex w-full justify-between  py-8">
+                        <ul className="marker:text-sky-400 text-center">
+                            <li>Autor: {data?.autor?.nome}</li>
+                            <li>Email: {data?.autor?.email}</li>
+                            <li>Linkedin: {data?.autor?.perfil_social.linkedin}</li>
+                            <li>Twitter: {data?.autor?.perfil_social?.twitter}</li>
+                        </ul>
+                        <div>
+                            <span className="text-center flex justify-center">
+                                Data de Publicação: {data?.data_publicacao}
+                            </span>
+                        </div>
+                        <div className="flex items-center space-x-4">
+                            <label className="text-2xl">tags: </label>
+                            <div className="flex-col">
+                                {data?.tags?.map((item, index) => {
+                                    return (
+                                        <div key={index}>
+                                            {item}
+                                        </div>
+                                    )
+                                })}
                             </div>
-                        )
-                    })} */}
-                </div>
-                {/* <div className="w-full max-h-[200px] overflow-auto overflow-y-hidden my-8">
-                    <Image src={data[0].midia.nome} width={1920} height={600} alt={"ImageServer"} priority={true} />
-                </div>
-                <div className="flex-col justify-center mb-8">
-                    {data.map((item, index) => {
-                        return (
-                            <div key={index + item.id} >
-                                <div className="text-center py-8">
-                                    <h2>{item.title}</h2>
-                                </div>
-                                <div className="w-11/12 m-auto">
-                                    <p>{item.resposta}</p>
-                                </div>
+                        </div>
+                    </div>
+                    <div className="flex justify-between">
+                        <div className="w-full">
+                            <h3 className="text-2xl text-center">
+                                Comentarios
+                            </h3>
+                            <div className="space-y-4 p-2">
+                                {data?.comentarios?.map((item, index) => {
+                                    return (
+                                        <div key={index}>
+                                            <div className="bg-neutral-600 border-2 border-blue-600 rounded-md p-4">
+                                                <ul>
+                                                    <li>Autor: {item.autor_comentario}</li>
+                                                    <li>Email: {item.email_comentario}</li>
+                                                    <li>Data: {item.data_comentario}</li>
+                                                    <li> Comentário: {item.texto_comentario}</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    )
+                                })}
                             </div>
-                        )
-                    })}
-                </div> */}
+                        </div>
+                        <div className="w-full">
+                            <h3 className="text-2xl text-center">
+                                Artigos Relacionados
+                            </h3>
+                            <div className="space-y-4 p-2">
+                                {data?.artigos_relacionados?.map((item, index) => {
+                                    return (
+                                        <div key={index}>
+                                            <div className="bg-neutral-600 border-2 border-blue-600 rounded-md p-4">
+                                                <ul>
+                                                    <h4 className="text-xl justify-center flex">{item.titulo}</h4>
+                                                    <div className="flex-col">
+                                                        <label className="text-center flex justify-center">Resumo</label>
+                                                        <div className="p-4">{item.resumo}</div>
+                                                    </div>
+                                                    <div className="flex justify-between">
+                                                        <span>Autor: {item.autor}</span>
+                                                        <span>Data: {item.data_publicacao}</span>
+                                                    </div>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <h5 className="text-2xl py-4">Informações Adicionais</h5>
+                        <ul className="py-4">
+                            <li>Categoria: {data?.categoria}</li>
+                            <li>Visualizações: {data?.visualizacoes}</li>
+                            <li>Tempo de Leitura:  {data?.tempo_leitura}</li>
+                            <li>Idioma: {data?.idioma}</li>
+                            <li>Número de Palavras: {data?.numero_palavras}</li>
+                            <li>Foto do Autor: {data?.autor_foto}</li>
+                            <li>Url de compartilhamento: {data?.url_compartilhamento}</li>
+                            <li>Referencia: {data?.fonte_referencia}</li>
+                        </ul>
+                    </div>
+                </div>
             </ContainerPage>
         </div>
     )
